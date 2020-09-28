a5d1e5431ae489dfa2284378b1ff002afa24a7e1
manju-minji <0923alswl@gmail.com>
2020-09-28 18:58:38 +0900

    과제3 조건 추가

--- multi-table.sh
+++ multi-table.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
-for i in $(seq 1 $1)
-do
-  for j in $(seq 1 $2)
+if [ $1 -le 0 ]
+then
+  echo "첫번째 입력값의 범위가 적절하지 않습니다."
+  exit 0
+elif [ $2 -le 0 ]
+then
+  echo "두번째 입력값의 범위가 적절하지 않습니다." 
+  exit 0
+else
+  for i in $(seq 1 $1)
   do
-    num=`expr $i \* $j`
-    echo -n $i'*'$j'='$num'  ' 
+    for j in $(seq 1 $2)
+    do
+      num=`expr $i \* $j`
+      echo -n $i'*'$j'='$num'  ' 
+    done
+    echo
   done
-  echo
-done
+fi
 exit 0
